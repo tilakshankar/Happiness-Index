100cb2a42a4bd7f71c781c15acc9049f13ef4d34
Dinesh <dineshraja4102@gmail.com>
2023-04-25 17:48:28 +0530

    added script

---
+++ script.js
@@ -0,0 +1,37 @@
+function test()
+{
+    var data= document.getElementsByName("meet");
+    
+
+    for(var i=0;i<data.length;i++)
+    {
+        if(data[i].checked)
+        {
+            val=localStorage.setItem("Question1",data[i].value);
+        }
+      }
+
+    var store=localStorage.getItem("Question1",val);
+
+
+
+    var data1=document.getElementsByName("involved");
+
+    for(var i=0;i<data1.length;i++)
+    {
+        if(data1[i].checked)
+        {
+            val=localStorage.setItem("Question2",data1[i].value);
+        }
+      }
+
+    var store1=localStorage.getItem("Question2",val);
+
+    }
+
+function results()
+{
+    var result= store*store1;
+
+    document.write("Happiness Index is:"+ result);
+}
